245c2da0b39428514f38cf6fc01d7bb94b6ef8f8
Ashish R <107366609+Username1unavailable@users.noreply.github.com>
2025-11-06 08:49:52 -0800

    Update Firebase configuration with placeholders
    Replaced Firebase configuration values with placeholders.

--- src/ firebase.js
+++ src/ firebase.js
@@ -5,13 +5,13 @@ import { getAuth, setPersistence, browserLocalPersistence, GoogleAuthProvider } 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCG4d0tvKVqdfFWg-0WvxxyVg5WxrpoMRk",
-  authDomain: "bubel-66fad.firebaseapp.com",
-  projectId: "bubel-66fad",
-  storageBucket: "bubel-66fad.appspot.com",
-  messagingSenderId: "1076211171958",
-  appId: "1:1076211171958:web:32d85a0d59f1c0fb625557",
-  measurementId: "G-FY9PFP4X5Y"
+  apiKey: "replace",
+  authDomain: "replace",
+  projectId: "replace",
+  storageBucket: "replace",
+  messagingSenderId: "replace",
+  appId: "replace",
+  measurementId: "replace"
 };
 
 const app = initializeApp(firebaseConfig);
